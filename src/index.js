223dd7c37ffb83215b6aac2cbece1445b7278232
Phil198705 <Arturik2009phil@mail.ru>
2023-02-26 18:27:14 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.flat();
+  let stack = [];
+  let res = 0;
+  let openBrackets = ['(', '{', '[']
+  for( i = 0; i < str.length; i++){
+    if((openBrackets.includes(str[i]) && bracketsConfig.includes(str[i]))){
+      stack.push(str[i]);
+    }else if(str[i] == '|' && res == 0){
+      stack.push(str[i]);
+    }else{
+      if(stack.length === 0){
+        return false;
+      }
+    }
+    if((stack[stack.length - 1] === '(' && str[i] === ')') ||
+     (stack[stack.length - 1] === '[' && str[i] === ']') || 
+     (stack[stack.length - 1] === '{' && str[i] === '}') || 
+     (stack[stack.length - 1] === '|' && str[i] === '|')){
+      stack.pop(str[i]);
+    }
+  
+  } return stack.length === 0;
 }
